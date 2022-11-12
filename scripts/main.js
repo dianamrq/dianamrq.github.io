79819daa338efc2945c4053b8bba0ee9546f2c6e
Ricardo Rouco <ricardo.rouco@gmail.com>
2022-11-12 15:51:30 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,4 @@ AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000
 });
-// Calculate days since Dec 1st 2012
-var initialDate = new Date(1990,1,1); // Dec 1st 2012
-var now = Date.now();
-var difference = (now - initialDate);
-var millisecondsPerDay = 24 * 60 * 60 * 1000;
-var daysSince = Math.floor(difference / millisecondsPerDay/365);
 
-// Write result to HTML
-document.getElementById('days_since').innerHTML = daysSince;
